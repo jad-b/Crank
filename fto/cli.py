314d846ab18e19603e6a0cb7affa605ec7aefde8
jad-b <j.american.db@gmail.com>
2014-10-24 07:32:30 -0400

    Add week and increment to cli

--- fto/cli.py
+++ fto/cli.py
@@ -15,10 +15,15 @@ def process_input(units='lbs'):
         .strip(string.whitespace)
     kgs = input("Kilograms? y or n: ").strip(string.whitespace)
     weight = int(input("Enter last week's max weight: "))
-    week = int(input("Enter current training week:"))
+    week = int(input("Enter current training week: "))
+    if week == 1:
+        increment = int(input("How much are we adding? "))
+    else:
+        increment = 0
+    units = MassUnit.kgs if kgs == 'y' else MassUnit.lbs
 
-    units = MassUnit.kgs if kgs else MassUnit.lbs
-    print_exercise(name, weight, week, units)
+    print_exercise(name, weight, week, units, increment)
+
 
 if __name__ == '__main__':
             process_input()
