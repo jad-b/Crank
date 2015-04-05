ddb8b4385848eed0a3cd3e22c77d892d63a0afe5
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2015-04-05 10:25:52 -0400

    Fix fto.cli increment issue.

--- crank/fto/cli.py
+++ crank/fto/cli.py
@@ -17,12 +17,11 @@ def process_input(units='lbs'):
     units = MassUnit.kgs if kgs == 'y' else MassUnit.lbs
     week = int(input("Enter current training week: "))
 
+    weight = int(input("Enter max weight: "))
     if week == 1:
-        prev_weight = int(input('Enter previous weight: '))
         increment = int(input("How much are we adding? "))
-        weight = get_max_from_previous(prev_weight, week, increment, units)
+        weight += increment
     else:
-        weight = int(input("Enter max weight: "))
         increment = 0
 
     print_exercise(name, weight, week, units)
