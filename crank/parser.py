4cc2a8b33a12e3315fe969ce0dbd47035aceb964
jad-b <j.american.db@gmail.com>
2015-10-23 12:27:01 -0400

    WIP recursive error resolution post-parse

--- crank/parser.py
+++ crank/parser.py
@@ -6,6 +6,7 @@ import ipdb
 from ipdb import launch_ipdb_on_exception
 
 logger = logging.getLogger(__name__)
+logger.propagate = False
 logger.setLevel(logging.DEBUG)
 sh = logging.StreamHandler()
 sh.setLevel(logging.DEBUG)
@@ -16,10 +17,27 @@ sh.setFormatter(log_fmt)
 logger.addHandler(sh)
 
 
-class ParseError(Exception):
-    def __init__(self, message, callback):
-        super().__init__(message)
+class ParseCallback:
+
+    def __init__(self, field, value, callback):
+        self.field = field
+        self.value = value
         self.callback = callback
+
+    def resolve(self):
+        # Display incorrect field
+        print("{}: {}".format(self.field, self.value))
+        done = 'no'
+        while done not in ('y', 'yes'):
+            # Request user correction
+            redo = input('Please enter the corrected value: ').strip()
+            # Verify input
+            done = input('Does this look right? "{}" '.format(redo))
+        # Apply callback
+        return self.callback(redo)
+
+    def __str__(self):
+        return "{}: {}".format(self.field, self.value)
 
 
 def parse_workouts(wkt_src):
@@ -40,42 +58,52 @@ def parse_workout(lines):
     # Exercises
     wkt['exercises'] = exs = []
     while len(lines) > 0:
-        ex, sets, tags, lines = parse_exercise(lines)
-        exs.append({'name': ex, 'sets': sets, 'tags': tags})
+        ex, lines = parse_exercise(lines)
+        exs.append(ex)
     return wkt
 
 
 def parse_timestamp(line):
     logger.debug(line)
-    ts_format = '%Y %b %d @ %H%M'
-    try:
-        ts = datetime.strptime(line, ts_format)
-    except:
-        return ParseError('Timestamp: {}'.format(line), parse_timestamp)
-    logger.info(ts)
-    return ts
+    ts_formats = (
+        '%Y %b %d @ %H%M',
+        '%d %b %Y @ %H%M',
+        '%d %B %Y @ %H%M',
+    )
+
+    for fmt in ts_formats:
+        try:
+            return datetime.strptime(line, fmt)
+        except:
+            pass
+    else:
+        return ParseCallback('Timestamp', line, parse_timestamp)
 
 
 def parse_exercise(lines):
     logger.debug("Exercise: %s", lines)
     logger.debug("Before: %s", lines[0])
     # Exercise name & sets
-    name, sets = parse_exercise_name(lines[0])
-    logger.info('Name: %s, Sets: %s', name, sets)
+    ex = {}
+    ex['name'], ex['sets'] = parse_exercise_name(lines[0])
+    lines = lines[1:]
+    logger.info('Name: %s, Sets: %s', ex['name'], ex['sets'])
     # Tags
     if len(lines) > 1:
-        tags, lines = parse_tags(lines[1:])
-    else:
-        return name, sets, {}, []
+        ex['tags'], lines = parse_tags(lines)
     logger.debug("Remaining: %s", lines)
-    return name, sets, tags, lines
+    return ex, lines
 
 
 def parse_exercise_name(line):
     try:
-        return lines[0].split(':')
-    except:
-        return ParseError('Exercise: {}'.format(lines[0]), parse_exercise), ''
+        name_sets = line.split(':')
+        if len(name_sets) != 2:
+            return ParseCallback('exercise name', line, parse_exercise), ''
+        return name_sets[0], name_sets[1]
+    except Exception as e:
+        logger.exception('Error while parsing exercise')
+        return ParseCallback('exercise name', line, parse_exercise), ''
 
 
 def parse_tags(lines):
@@ -93,11 +121,11 @@ def parse_tags(lines):
     return tags, lines[len(tags):]
 
 
-def parse_tag(line)
+def parse_tag(line):
     try:
         return line.lstrip('- ').split(':')
     except:
-        return ParseError('Tag: {}'.format(line), parse_tag)
+        return ParseCallback('Tag: {}'.format(line), parse_tag)
 
 
 def stream_blocks(filename):
@@ -126,18 +154,15 @@ def buffer_blocks(source):
 def dfs(nest, fn):
     for i in nest:
         # Keep recurring
-        if isinstance(i, list) or isinstance(i, dict):
+        if isinstance(nest, list):
             dfs(i, fn)
+        if isinstance(nest, dict):
+            dfs(nest[i], fn)
         # not a list or dict; evaluate for error
         else:
-            if isinstance(nest[i], ParseError):
-                nest[i] = resolve_error(nest[i])
+            fn(nest, i)
 
 
-def resolve_error(err):
-    print(err.args[0])
-    redo = input('Please enter the corrected value: ').strip()
-    done = 'no'
-    while done not in ('y', 'yes'):
-        done = input('Does this look right? {}'.format(redo))
-    return redo
+def resolve_errors(struct, idx):
+    if isinstance(struct[i], ParseCallback):
+        struct[i] = struct[i].callback()
